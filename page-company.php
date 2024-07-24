94d9877fb6b3719105fd6746d5b22b4d2b2d316d
Eror <eror.ferizi@pulla.digital>
2024-07-24 10:45:24 +0200

    About us values style done

--- page-company.php
+++ page-company.php
@@ -9,6 +9,7 @@ get_header();
 			get_template_part( 'template-parts/content', 'we-are' );        
 			get_template_part( 'template-parts/content', 'our-services' );        
 			get_template_part( 'template-parts/content', 'awards' );        
+			get_template_part( 'template-parts/content', 'values' );        
 
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
---
+++ template-parts/content-values.php
@@ -0,0 +1,59 @@
+<section class="values">
+    <div class="wraper-values">
+        <h1 class="text-principles">Our pRinciplEs</h1>
+        <h1 class="text-values">and valuEs</h1>
+        <div class="inside-values">
+            <div class="section-2">
+                <img src="<?php echo get_template_directory_uri() . '/imgs/Rectangle-1.png'; ?>" alt="">
+                <div class="wrap-text-value">
+                    <p class="val">/ values /</p>
+                    <p class="text">“At, we're on a mission to transform the digital landscape. We're a dynamic team of experts driven”.</p>
+                </div>
+            </div>
+            <div class="section-1">
+                <img src="<?php echo get_template_directory_uri() . '/imgs/Rectangle-2.png'; ?>" alt="">
+                <div class="wrap-text-value">
+                    <p class="val">/ values /</p>
+                    <p class="text">“At, we're on a mission to transform the digital landscape. We're a dynamic team of experts driven”.</p>
+                </div>
+            </div>
+             <div class="section-3">
+                <img src="<?php echo get_template_directory_uri() . '/imgs/Rectangle-2.png'; ?>" alt="">
+                <div class="wrap-text-value">
+                    <p class="val">/ values /</p>
+                    <p class="text">“At, we're on a mission to transform the digital landscape. We're a dynamic team of experts driven”.</p>
+                </div>
+            </div>
+             <div class="section-4">
+                <img src="<?php echo get_template_directory_uri() . '/imgs/Rectangle-1.png'; ?>" alt="">
+                <div class="wrap-text-value">
+                    <p class="val">/ values /</p>
+                    <p class="text">“At, we're on a mission to transform the digital landscape. We're a dynamic team of experts driven”.</p>
+                </div>
+            </div>
+             <div class="section-5">
+                <img src="<?php echo get_template_directory_uri() . '/imgs/Rectangle-1.png'; ?>" alt="">
+                <div class="wrap-text-value">
+                    <p class="val">/ values /</p>
+                    <p class="text">“At, we're on a mission to transform the digital landscape. We're a dynamic team of experts driven”.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="stats">
+    <div class="wraper-stats">
+        <div class="inside-stats">
+            <div class="wraper-inside-text">
+                <h5>3D Artists and Producers, who play as an extended team for Creative Studios and Advertising Agencies since 2005. We are a small company with around 20 in house people, which allows us to stay design-driven.</h5>
+                <div class="texts">
+                    <p class="left-right">We are a small company with around 20 in house people, which allows us to stay design-driven and help bigger companies achieve motion graphics that inspire, and visual effects that make a difference. We are a small company with around 20 in house people.</p>
+                    <p class="left-right">We are a small company with around 20 in house people, which allows us to stay design-driven and help bigger companies achieve motion graphics that inspire, and visual effects that make a difference.</p>
+                </div>
+            </div>
+            <div class="img-wrap">
+                <img src="<?php echo get_template_directory_uri() . '/imgs/Rectangle-2.png'; ?>" alt="">            </div>
+        </div>
+    </div>
+</section>
