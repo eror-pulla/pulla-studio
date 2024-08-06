f782e43a5eff311c7ddf6b07cf2cda5c4565b7b2
Eror <eror.ferizi@pulla.digital>
2024-08-06 14:10:08 +0200

    Socials in About

--- template-parts/content-social-media.php
+++ template-parts/content-social-media.php
@@ -1,65 +1,3 @@
-<!-- <section class="social-media">
-    <div class="wraper-social">
-        <div class="inside-social">
-            <h1>SOcial mEdia</h1>
-            <div class="socials">
-                <ul>
-                    <div class="socials-wraper">
-                        <p class="nr">00-1</p>
-                        <p class="name">Instagram</p>
-                        <div class="go-to">
-                            <a href="">
-                                <p>Go to instagram</p>
-                                <img src="<?php echo get_template_directory_uri() . '/imgs/Arrow.png'; ?>" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="socials-wraper">
-                        <p class="nr">00-2</p>
-                        <p class="name">Linkedin</p>
-                        <div class="go-to">
-                            <a href="">
-                                <p>Go to Linkedin</p>
-                                <img src="<?php echo get_template_directory_uri() . '/imgs/Arrow.png'; ?>" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="socials-wraper">
-                        <p class="nr">00-3</p>
-                        <p class="name">Facebook</p>
-                        <div class="go-to">
-                            <a href="">
-                                <p>Go to Facebook</p>
-                                <img src="<?php echo get_template_directory_uri() . '/imgs/Arrow.png'; ?>" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="socials-wraper">
-                        <p class="nr">00-4</p>
-                        <p class="name">Behance</p>
-                        <div class="go-to">
-                            <a href="">
-                                <p>Go to Behance</p>
-                                <img src="<?php echo get_template_directory_uri() . '/imgs/Arrow.png'; ?>" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="socials-wraper">
-                        <p class="nr">00-5</p>
-                        <p class="name">Vimeo</p>
-                        <div class="go-to">
-                            <a href="">
-                                <p>Go to Vimeo</p>
-                                <img src="<?php echo get_template_directory_uri() . '/imgs/Arrow.png'; ?>" alt="">
-                            </a>
-                        </div>
-                    </div>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section> -->
-
 <section class="social-media">
     <div class="wraper-social">
         <div class="inside-social">
@@ -76,7 +14,7 @@
                                 <p class="nr"><?php echo sprintf('00-%d', get_row_index()); ?></p>
                                 <p class="name"><?php echo esc_html($name); ?></p>
                                 <div class="go-to">
-                                    <a href="<?php echo esc_url($url); ?>">
+                                    <a href="<?php echo esc_url($url); ?>" target="_blank" rel="noopener noreferrer">
                                         <p>Go to <?php echo esc_html($name); ?></p>
                                         <img src="<?php echo get_template_directory_uri() . '/imgs/Arrow.png'; ?>" alt="">
                                     </a>
